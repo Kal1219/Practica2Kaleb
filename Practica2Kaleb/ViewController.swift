09db3c318d15380dfc15e291a0670aecd8169f6a
Kal1219 <kaleb.cortes1208@gmail.com>
2023-08-23 10:37:37 -0600

    Swift UI con una lista de imágenes que ciclan con un botón en swift playground

--- Practica2Kaleb/ViewController.swift
+++ Practica2Kaleb/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let listaDeElementos = ["gohan2", "goku ss3", "majin vegeta", "gogeta", "vegito", "goku ss1", "gotenks", "trunks", "bardock", "goku ui"]
+    var elementosIndice = 0
+    
+    @IBOutlet weak var imagen: UIImageView!
+    @IBOutlet weak var etiqueta: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        actualizaElemento()
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func mostrar(_ sender: Any) {
+        etiqueta.text = listaDeElementos[elementosIndice]
+    }
+    
+    @IBAction func siguiente(_ sender: Any) {
+        elementosIndice += 1
+        if (elementosIndice >= listaDeElementos.count)
+        {
+            elementosIndice = 0
+        }
+        actualizaElemento()
+    }
+    
+    func actualizaElemento()
+    {
+        etiqueta.text = "?"
+        let nombreDeElemento = listaDeElementos[elementosIndice]
+        let laImagen = UIImage(named: nombreDeElemento)
+        imagen.image = laImagen
+    }
 
 }
 
